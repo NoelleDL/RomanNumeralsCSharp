29b29deb9e9c675ddde30ca9f7844c3961dd03f6
NoelleDL <noelle.legrain@gmail.com>
2018-07-05 16:24:37 +0100

    First test and method

---
+++ MoreRomanNumerals.Library.Tests/ConverterTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework; 
+using System;
+
+
+namespace MoreRomanNumerals.Library.Tests
+{
+    [TestFixture()]
+    public class ConverterTests
+    {
+        [Test()]
+        public void ReturnsI_When1()
+        {
+            int input = 1;
+            string output = Converter.Convert(input);
+            Assert.AreEqual("I", output);
+        }
+    }
+}
---
+++ MoreRomanNumerals.Library/Converter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MoreRomanNumerals.Library
+{
+    public class Converter
+    {
+        public static string Convert(int input)
+        {
+            string output = "I";
+            return output;
+        }
+    }
+}
